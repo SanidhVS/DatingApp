dd2496a9a56f3fd1aaa28eb55f96ac825c7fff9f
sanidh.vs <sanidh.vs@experionglobal.com>
2021-08-12 23:49:22 +0530

    End of section 6

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [  //This represents the different routing that we want, and its here that we declare different paths and where should it direct to
+  {path: '', component: HomeComponent}, //Will route to home component when there is no /
+  {
+    path : '',
+    runGuardsAndResolvers : 'always',
+    canActivate : [AuthGuard], //Will route to MemberListComponent, AuthGuard is used for preventing unauthorized access
+    children : [
+      {path: 'members', component: MemberListComponent}, 
+      {path: 'members/:id', component: MemberDetailComponent}, //Will route to MemberDetailComponent
+      {path: 'lists', component: ListsComponent}, //Will route to ListsComponent
+      {path: 'messages', component: MessagesComponent}, //Will route to MessagesComponent
+
+    ]
+  },
+
+  {path: '**', component: HomeComponent, pathMatch : 'full'}, //Will route to home component if theres any wrong URL
+
+
+
+]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,7 +14,7 @@ export class NavComponent implements OnInit {
 
   model : any = {}; //Empty object
   currentUser$ : Observable<User> //Initializes an observable for async pipe
-  constructor(private accountService : AccountService  ) { }  //AccountService is injected into the ctor so that we can make use of the service and make a post req to API
+  constructor(private accountService : AccountService, private router : Router, private toastr : ToastrService  ) { }  //AccountService is injected into the ctor so that we can make use of the service and make a post req to API
 
   ngOnInit(): void {
     this.currentUser$ = this.accountService.currentUser$; //This will maintain the observable that is used at the login 
@@ -21,9 +23,10 @@ export class NavComponent implements OnInit {
   login(){
      this.accountService.login(this.model).subscribe( //since it is an observable we have to use subscribe as it is lazy
        response => {
-         console.log(response);
+         this.router.navigateByUrl("/members"); //To route to members page on logging in
        }, error => {
          console.log(error);
+         this.toastr.error(error.error);
          
        }
      )
@@ -31,6 +34,7 @@ export class NavComponent implements OnInit {
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl("/"); //To navigate to home page on logging out
   }
 
 
